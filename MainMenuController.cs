5438dc1c8e71d46721283e425e959c7ffddb10af
hwxz8cms49-ux <hwxz8cms49@privaterelay.appleid.com>
2026-05-25 17:46:09 +0300

    MainMenuController.cs

--- MainMenuController.cs
+++ MainMenuController.cs
@@ -3,8 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour 
 {
-   public void PlayGame()
+    public GameObject ModeSelection;
+    private void Start()
     {
+        if (ModeSelection != null) 
+        { 
+            ModeSelection.SetActive(false); 
+        }
+    }
+    public void OpenSelectionPanel()
+    {
+        if (ModeSelection != null)
+        {
+            ModeSelection.SetActive(true);
+        }
+    }
+    public void NormalMode()
+    {
+        PlayerPrefs.SetString("GameMode", "Normal");
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void TimerMode()
+    {
+        PlayerPrefs.SetString("GameMode", "Timer");
+        PlayerPrefs.Save();
         SceneManager.LoadScene("SampleScene");
     }
     public void StopGame()
